892bc19083cce60665679d66bd85ecdf07a4bf4a
Victor Gouvêa <victorgouvea28@gmail.com>
2024-08-09 16:30:13 -0300

    Github actions to deploy lambda

--- commands/publish_comands.py
+++ commands/publish_comands.py
@@ -1,10 +1,11 @@
 from typing import Final
-from dotenv import load_dotenv
 import requests
 import yaml
 import os
 
-load_dotenv()
+# Uncomment for local testing
+# from dotenv import load_dotenv
+# load_dotenv()
 
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 APPLICATION_ID: Final[str] = os.getenv('DISCORD_APPLICATION_ID')
@@ -13,23 +14,24 @@ URL = f"https://discord.com/api/v9/applications/{APPLICATION_ID}/commands"
 def yaml_to_json(file_path: str):
     with open(file_path, "r") as file:
         yaml_content = file.read()
-    
+
     commands = yaml.safe_load(yaml_content)
-    
+
     return commands
 
 def publish_commands(file_path: str):
     commands = yaml_to_json(file_path)
     headers = {"Authorization": f"Bot {TOKEN}", "Content-Type": "application/json"}
-    
+
     # Send the POST request for each command
     for command in commands:
         response = requests.post(URL, json=command, headers=headers)
         command_name = command["name"]
+        # No futuro adicionar tratamento de erro
         print(f"Command {command_name} created: {response.status_code}")
 
 def main():
     publish_commands("bot_commands.yaml")
-    
+
 if __name__ == "__main__":
     main()
--- src/main.py
+++ src/main.py
@@ -3,9 +3,10 @@ from flask import Flask, jsonify, request
 from mangum import Mangum
 from asgiref.wsgi import WsgiToAsgi
 from discord_interactions import verify_key_decorator
-from dotenv import load_dotenv
 
-load_dotenv()
+# Uncomment for local testing
+# from dotenv import load_dotenv
+# load_dotenv()
 
 DISCORD_PUBLIC_KEY = os.getenv("DISCORD_PUBLIC_KEY")
 
@@ -23,29 +24,31 @@ async def interactions():
 @verify_key_decorator(DISCORD_PUBLIC_KEY)
 def interact(raw_request):
     if raw_request["type"] == 1:
-        response_data = {"type": 1}
+        return jsonify({"type": 1})
+
+    data = raw_request["data"]
+    command_name = data["name"]
+
+    if command_name == "hello":
+        message_content = "Hello there!"
+    elif command_name == "about":
+        message_content = "I am a bot made to help you with your F1 needs!"
+    elif command_name == "song":
+        message_content = "The Dutch National Anthem never leaves my playlist!"
+    elif command_name == "winner":
+        message_content = "I don't have this info right now, but it should be Max Verstappen..."
+    elif command_name == "echo":
+        original_message = data["options"][0]["value"]
+        message_content = f"Echoing: {original_message}"
     else:
-        data = raw_request["data"]
-        command_name = data["name"]
-        
-        if command_name == "hello":
-            message_content = "Hello there!"
-        elif command_name == "song":
-            message_content = "The Dutch National Anthem never leaves my playlist!"
-        elif command_name == "winner":
-            message_content = "I don't have this info right now, but it should be Max Verstappen..."
-        elif command_name == "echo":
-            original_message = data["options"][0]["value"]   
-            message_content = f"Echoing: {original_message}"
-        else:
-            message_content = "I don't understand this command, try again!"
-            
-        response_data = {
-            "type": 4,
-            "data": {"content": message_content}
-        }
-        
+        message_content = "I don't understand this command, try again!"
+
+    response_data = {
+        "type": 4,
+        "data": {"content": message_content}
+    }
+
     return jsonify(response_data)
-    
+
 if __name__ == "__main__":
     app.run(debug=True)
