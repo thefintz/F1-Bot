19b34671b65edb8bfb4f9b8eea80d988db8eea2a
Victor Gouvêa <victorgouvea28@gmail.com>
2024-08-09 18:09:21 -0300

    Reverting API changes

--- src/main.py
+++ src/main.py
@@ -11,6 +11,8 @@ from discord_interactions import verify_key_decorator
 DISCORD_PUBLIC_KEY = os.getenv("DISCORD_PUBLIC_KEY")
 
 app = Flask(__name__)
+asgi_app = WsgiToAsgi(app)
+handler = Mangum(asgi_app)
 
 @app.route("/", methods=["POST"])
 async def interactions():
@@ -22,34 +24,31 @@ async def interactions():
 @verify_key_decorator(DISCORD_PUBLIC_KEY)
 def interact(raw_request):
     if raw_request["type"] == 1:
-        return jsonify({"type": 1})
-
-    data = raw_request["data"]
-    command_name = data["name"]
-
-    if command_name == "hello":
-        message_content = "Hello there!"
-    elif command_name == "about":
-        message_content = "I am a bot made to help you with your F1 needs!"
-    elif command_name == "song":
-        message_content = "The Dutch National Anthem never leaves my playlist!"
-    elif command_name == "winner":
-        message_content = "I don't have this info right now, but it should be Max Verstappen..."
-    elif command_name == "echo":
-        original_message = data["options"][0]["value"]
-        message_content = f"Echoing: {original_message}"
+        response_data = {"type": 1}
     else:
-        message_content = "I don't understand this command, try again!"
-
-    response_data = {
-        "type": 4,
-        "data": {"content": message_content}
-    }
-
+        data = raw_request["data"]
+        command_name = data["name"]
+        
+        if command_name == "hello":
+            message_content = "Hello there!"
+        elif command_name == "about":
+            message_content = "I am a bot made to help you with your F1 needs!"
+        elif command_name == "song":
+            message_content = "The Dutch National Anthem never leaves my playlist!"
+        elif command_name == "winner":
+            message_content = "I don't have this info right now, but it should be Max Verstappen..."
+        elif command_name == "echo":
+            original_message = data["options"][0]["value"]   
+            message_content = f"Echoing: {original_message}"
+        else:
+            message_content = "I don't understand this command, try again!"
+            
+        response_data = {
+            "type": 4,
+            "data": {"content": message_content}
+        }
+        
     return jsonify(response_data)
-
-asgi_app = WsgiToAsgi(app)
-handler = Mangum(asgi_app)
-
+    
 if __name__ == "__main__":
     app.run(debug=True)
